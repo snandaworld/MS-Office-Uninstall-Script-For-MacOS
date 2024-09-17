722e25d830cc5a7a2a824185e51bd3c13ee31c34
Shivanand Anchalkar <snandaworld@hotmail.com>
2024-09-17 16:43:33 +0530

    Create inputtest.sh

---
+++ inputtest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Prompt the user for a number
+echo "Please enter a number:"
+read user_number
+
+# Check if the number is greater than 10
+if [ "$user_number" -gt 10 ]; then
+    echo "The number you entered is greater than 10."
+else
+    echo "The number you entered is 10 or less."
+fi
