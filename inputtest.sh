d2071cea690d96ed51838c83a0dad7f5280aa6ed
Shivanand Anchalkar <snandaworld@hotmail.com>
2024-09-17 17:13:36 +0530

    Update inputtest.sh

--- inputtest.sh
+++ inputtest.sh
@@ -2,8 +2,9 @@
 
 # Prompt the user for a number
 echo "Please enter a number:"
-read user_number
 sleep 6
+read user_number
+
 # Check if the number is greater than 10
 if [ "$user_number" -gt 10 ]; then
     echo "The number you entered is greater than 10."
