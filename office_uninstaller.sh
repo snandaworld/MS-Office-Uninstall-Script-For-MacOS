2ba3a8dd8b990f36bfe350b95bb15d4c68318759
Shivanand Anchalkar <snandaworld@hotmail.com>
2025-02-24 15:31:20 +0530

    Update office_uninstaller.sh

--- office_uninstaller.sh
+++ office_uninstaller.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 USERNAME=$(ls -l /dev/console | awk '{print $3}')
 BACKUPPATH="~/Documents/Outlook_Backup"
-SOURCEPATH="/Users/$USERNAME/Library/Group Containers/UBF8T346G9.Office/Outlook/"
+SOURCEPATH=/Users/$USERNAME/Library/Group\ Containers/UBF8T346G9.Office/Outlook/
 
 if [[ $EUID -ne 0 ]]; then
     echo -e "
@@ -17,6 +17,7 @@ else
     ###################################
     "
         echo "Backup initiated..."
+        echo "Backup from $SOURCEPATH"
         # Create the backup directory (if not already present)
         mkdir -p $BACKUPPATH
  
